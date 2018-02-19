3e9ae28048acf3f7c1ff6cdb0817eabde9832d22
phil7394 <philipthomas7394@gmail.com>
2018-02-19 10:58:45 -0600

    added comments

--- FileParser.py
+++ FileParser.py
@@ -1,6 +1,7 @@
 import re
 
 
+# parse parameters
 def parse_params(params_file_path):
     params_file = open(params_file_path)
     mis = {}
@@ -10,12 +11,10 @@ def parse_params(params_file_path):
     for line in params_file:
         m = re.search('MIS\((\d+)\)\s=\s(\d*\.\d*)', line)
         if m is not None:
-            # print "%s : %s" % (m.group(1), m.group(2))
             mis[m.group(1)] = float(m.group(2))
         else:
             m = re.search('SDC\s=\s(\d*.\d)', line)
             if m is not None:
-                # print "sdc : %s" % m.group(1)
                 sdc = float(m.group(1))
             else:
                 m = re.search('cannot_be_together:\s(.*)\r', line)
@@ -25,7 +24,6 @@ def parse_params(params_file_path):
                     items_str = m.group(1).replace('}, {', '};{')
                     item_sets = items_str.split(';')
                     for i in item_sets:
-                        # print i
                         p = i.replace('{', '')
                         q = p.replace('}', '')
                         cannot_be_together_list.append(q.split(', '))
@@ -37,13 +35,12 @@ def parse_params(params_file_path):
                     if m is not None:
                         must_haves = m.group(1).split(' or ')
                         for i in must_haves:
-                            # print i
                             must_haves_list.append(i)
-                            # print must_haves_list
 
     return mis, sdc, cannot_be_together_list, must_haves_list
 
 
+# parse transactions
 def parse_txns(input_file_path):
     txns_file = open(input_file_path)
     transactions = txns_file.read().replace('{', '').replace('}', '').split('\r\n')
--- MSApriori.py
+++ MSApriori.py
@@ -4,39 +4,31 @@ from collections import Counter
 import itertools
 
 
+# ms apriori algorithm
 def ms_apriori(transactions, mis, sdc):
-    # Ck = [('10', '20', '80'), ('20', '30', '80'), ('10', '120', '140')]
-    # Fk = []
     F = []
     supp_counts = {}
     tl_counts = {}
     M = sort(mis)
     N = len(transactions)
-    # print transactions
-    # print N
     L, sup_count = init_pass(transactions, M, mis, N)
     F.append(frequent_itemSets_1(L, sup_count, N))
-
     C = [x for x in F]
 
     k = 1  # NOTE: k run one number lower as array index start from 0
     while F[k - 1]:
-        # print k
-        # temp_count = {}
         Fk = []
         if k == 1:
             C.append(level2CandidateGen(L, sdc, sup_count, N, mis))
         else:
-            C.append(MSCandidateGen(F[k - 1], sdc, sup_count, N,  mis))
-        # print C
+            C.append(MSCandidateGen(F[k - 1], sdc, sup_count, N, mis))
         for txn in transactions:
-            visited = set()
+            visited = set()  # to remove duplicate tail counts
             for c in C[k]:
                 c = tuple(c)
                 if subset_check(txn, c):
                     if c not in supp_counts:
                         supp_counts[c] = 1
-
                     else:
                         supp_counts[c] += 1
 
@@ -48,7 +40,6 @@ def ms_apriori(transactions, mis, sdc):
                         else:
                             tl_counts[c[1:]] = tl_counts.get(c[1:]) + 1
 
-        # print temp_count
         for c in C[k]:
             c = tuple(c)
             if c in supp_counts and float(supp_counts[c]) / N >= mis[c[0]]:
@@ -56,21 +47,21 @@ def ms_apriori(transactions, mis, sdc):
         F.append(Fk)
         k += 1
 
-    # print "C: %s" % C
-    # print F['F1']
+    # add support counts of F1 to main supp_count dictionary
     for i in sup_count:
         supp_counts[tuple([i])] = sup_count[i]
     return F, supp_counts, tl_counts
 
 
-def subset_check(txn, c):
-    # print c, len(c)
-    diff_list = [item for item in c if item not in txn]
+# check if a is a subset of b
+def subset_check(b, a):
+    diff_list = [item for item in a if item not in b]
     if not diff_list:
         return True
     return False
 
 
+# filter frequent itemsets given special constraints
 def filter_constraints(freq_itemsets, cannot_be_together_list, must_haves_list):
     i_sets_to_remove = set()
     for fk in freq_itemsets:
@@ -78,9 +69,9 @@ def filter_constraints(freq_itemsets, cannot_be_together_list, must_haves_list):
             flag = False
             for cbt in cannot_be_together_list:
                 cbt_pair_list = list(itertools.combinations(cbt, 2))
-                # print cbt_pair_listtesting
                 for disjoint_items in cbt_pair_list:
-                    flag_d = subset_check(i_set, tuple([disjoint_items[0]])) and subset_check(i_set, tuple([disjoint_items[1]]))
+                    flag_d = subset_check(i_set, tuple([disjoint_items[0]])) and subset_check(i_set, tuple(
+                        [disjoint_items[1]]))
                     if flag_d:
                         i_sets_to_remove.add(i_set)
                 if must_haves_list:
@@ -93,10 +84,10 @@ def filter_constraints(freq_itemsets, cannot_be_together_list, must_haves_list):
         for fk in freq_itemsets:
             if i_set in fk:
                 fk.remove(i_set)
-    # print i_sets_to_remove
     return freq_itemsets
 
 
+# sort items by mis in ascending order
 def sort(mis):
     M = []
     for key, value in sorted(mis.iteritems(), key=lambda (k, v): (v, k)):
@@ -104,6 +95,7 @@ def sort(mis):
     return M
 
 
+# initial pass to generate L, support counts
 def init_pass(T, M, mis, N):
     L = []
     noItemInL = True
@@ -120,6 +112,7 @@ def init_pass(T, M, mis, N):
     return L, sup_count
 
 
+# generate F1
 def frequent_itemSets_1(L, sup_count, N):
     F1 = []
     for i in L:
@@ -128,6 +121,7 @@ def frequent_itemSets_1(L, sup_count, N):
     return F1
 
 
+# generate candidates for k = 2
 def level2CandidateGen(L, sdc, sup_count, N, mis):
     C2 = []
     for i in L:
@@ -135,11 +129,12 @@ def level2CandidateGen(L, sdc, sup_count, N, mis):
             indexOfI = L.index(i)
             postL = L[indexOfI + 1:]
             for h in postL:
-                if float(sup_count[h]) / N >= mis[i] and abs(float(sup_count[h])/N - float(sup_count[i])/N) <= sdc:
+                if float(sup_count[h]) / N >= mis[i] and abs(float(sup_count[h]) / N - float(sup_count[i]) / N) <= sdc:
                     C2.append(tuple([i, h]))
     return C2
 
 
+# generate candidates for k > 2
 def MSCandidateGen(Fk1, sdc, sup_count, N, mis):
     Ck = []
     Fk = []
@@ -148,7 +143,8 @@ def MSCandidateGen(Fk1, sdc, sup_count, N, mis):
         postF1 = Fk1[indexOfF1 + 1:]
         for f2 in postF1:
             if f1[:-1] == f2[:-1]:
-                if f1 and f2 and (f1[-1] < f2[-1] or f1[-1] > f2[-1]) and abs(float(sup_count[f1[-1]])/N - float(sup_count[f2[-1]])/N) <= sdc:
+                if f1 and f2 and (f1[-1] < f2[-1] or f1[-1] > f2[-1]) and abs(
+                        float(sup_count[f1[-1]]) / N - float(sup_count[f2[-1]]) / N) <= sdc:
                     c = list(f1)
                     c.append(f2[-1])
                     Ck.append(tuple(c))
@@ -156,11 +152,11 @@ def MSCandidateGen(Fk1, sdc, sup_count, N, mis):
                         if c[0] in s or mis[c[1]] == mis[c[0]]:
                             if s not in Fk1 and s in Ck:
                                 Ck.remove(s)
-    # print Ck
     return Ck
 
 
 if __name__ == '__main__':
+    # input agr parsing
     parser = argparse.ArgumentParser(description='MSApriori program for multiple item support (mis) apriori')
     optional = parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
@@ -170,37 +166,30 @@ if __name__ == '__main__':
     parser._action_groups.append(optional)
     args = vars(parser.parse_args())
 
-    mis, sdc, cannot_be_together_list, must_haves_list = FileParser.parse_params(args['params'])
-    txn_list = FileParser.parse_txns(args['input'])
-    freq_itemsets, support_counts, tail_counts = ms_apriori(txn_list, mis, sdc)
-    # print "freq_itemsets: %s" % freq_itemsets
-    # print "freq_itemsets length: %s" % len(freq_itemsets)
-    # freq_itemsets = [[('20'), ('40'), ('50'), ('10')], [('20', '40'), ('40', '70'), ('70', '80'), ('10', '120')],
-    #  [('20', '30', '80'), ('20', '40', '80'), ('30', '70', '80'), ('20', '40', '50')]]
-    filtered_freq_itemsets = filter_constraints(freq_itemsets, cannot_be_together_list, must_haves_list)
-    # print "filtered_freq_itemsets: %s" % filtered_freq_itemsets
-    # filtered_freq_itemsets = [[('20'), ('40'), ('50'), ('10')], [('20', '40'), ('40', '70'), ('70', '80'), ('10', '120')],
-    #                  [('20', '30', '80'), ('20', '40', '80'), ('30', '70', '80'), ('20', '40', '50')]]
-    # print "support_counts: %s" % support_counts
-    # print args
+    mis, sdc, cannot_be_together_list, must_haves_list = FileParser.parse_params(args['params'])  # parse params file
+    txn_list = FileParser.parse_txns(args['input'])  # parse transactions file
+    freq_itemsets, support_counts, tail_counts = ms_apriori(txn_list, mis, sdc)  # run ms apriori algorithm
+    filtered_freq_itemsets = filter_constraints(freq_itemsets, cannot_be_together_list,
+                                                must_haves_list)  # filter with special constraints
+    # open output file
     if args['output'] is not None:
         f = open(args['output'], 'w')
     else:
         f = open('output-patterns.txt', 'w')
 
+    # output format
     for i in range(len(filtered_freq_itemsets)):
         if filtered_freq_itemsets[i]:
             print "Frequent %s-itemsets\n" % str(i + 1)
             f.write("Frequent {}-itemsets\n\n".format(str(i + 1)))
             for item_set in filtered_freq_itemsets[i]:
                 sup = support_counts[item_set]
-                # sup = 5
                 if item_set[1:] in tail_counts:
                     tail_count = tail_counts[item_set[1:]]
                 else:
                     tail_count = None
-                # tail_count = None
-                is_str = ("{" + str(item_set).replace('(', '').replace(')', '').replace('\'', '') + "}").replace(',}', '}')
+                is_str = ("{" + str(item_set).replace('(', '').replace(')', '').replace('\'', '') + "}").replace(',}',
+                                                                                                                 '}')
 
                 print "\t\t%s : %s" % (sup, is_str)
                 f.write("\t\t{} : {}\n".format(sup, is_str))
@@ -209,10 +198,11 @@ if __name__ == '__main__':
                     f.write("Tailcount = {}\n".format(tail_count))
 
             print "\n\n\t\tTotal number of frequent %s-itemsets = %s\n\n" % (str(i + 1), len(filtered_freq_itemsets[i]))
-            f.write("\n\n\t\tTotal number of frequent {}-itemsets = {}\n\n\n".format(str(i + 1), len(filtered_freq_itemsets[i])))
+            f.write("\n\n\t\tTotal number of frequent {}-itemsets = {}\n\n\n".format(str(i + 1),
+                                                                                     len(filtered_freq_itemsets[i])))
         elif i is 0:
             print "Frequent %s-itemsets\n" % 1
             f.write("Frequent {}-itemsets\n\n".format(1))
             print "\n\t\tTotal number of frequent %s-itemsets = %s\n\n" % (1, 0)
             f.write("\n\t\tTotal number of frequent {}-itemsets = {}\n\n\n".format(1, 0))
-    f.close()
+    f.close()  # close output file
